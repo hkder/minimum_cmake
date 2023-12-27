96fccc99d060adccd27af0be7ef85d3528d45a9e
hkder <samhskim1@gmail.com>
2023-12-26 16:36:36 -0800

    small changes

--- src/db.cpp
+++ src/db.cpp
@@ -2,5 +2,6 @@
 
 DATABASE database[DATABASE_SIZE] = {
     {"DP_REGKEY_FIRST", 1},
-    {"DP_REGKEY_SECOND", 2}
+    {"DP_REGKEY_SECOND", 2},
+    {"DP_REGKEY_THIRD", 3}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 int main(int argc, char** argv)
 {
-    printf("key: %s, value: %d\n", database[0].regkey.c_str(), database[0].value);
-    printf("key: %s, value: %d\n", database[1].regkey.c_str(), database[1].value);
+    for(int i = 0; i < DATABASE_SIZE; ++i)
+    {
+        printf("index: %d, key: %s, value: %d\n", i, database[i].regkey.c_str(), database[i].value);
+    }
     return 0;
 }
